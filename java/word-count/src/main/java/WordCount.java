52c0ab4172e4165fc57dbfaac433e29888de0fe1
EricRicketts <eric_ricketts@me.com>
2022-02-01 16:05:55 -0500

    added comments about regular expression

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCount {
-
     public Map<String, Integer> phrase(String str) {
         List<String> allWords = wordFilter(str);
         return countWords(allWords);
     }
-
     private Map<String, Integer> countWords(List<String> listOfWords) {
         Map<String, Integer> wordCount = new HashMap<>();
-
         listOfWords.stream().forEach(word -> {
             if (wordCount.get(word) == null) {
                 wordCount.put(word.toLowerCase(), 1);
@@ -21,24 +21,14 @@ public class WordCount {
         });
         return wordCount;
     }
-
     private List<String> wordFilter(String str) {
         List<String> allMatchedWords = new ArrayList<>();
-        String stringPattern = "\\b[a-zA-Z\\d']+\\b";
+        String stringPattern = "\\b[a-zA-Z\\d']+\\b"; // search on the word boundary as we only get ' inside the word
         Pattern pattern = Pattern.compile(stringPattern);
         Matcher m = pattern.matcher(str);
-
         while (m.find()) {
             allMatchedWords.add(m.group().toLowerCase());
         }
-
         return allMatchedWords;
     }
 }
-/*
-    1.  replace any form of punctuation with a single space.
-    2.  split the result on \s+
-    3.  Form a regular expression which will pick out the words, put this into an array or list
-    4.  Can we use streams to count the unique words?
-    5.  Results must be put into a Map.
-*/
