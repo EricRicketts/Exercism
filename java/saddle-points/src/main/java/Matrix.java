9c52b909b51877f1213ab78c492bb2208b676bb3
EricRicketts <eric_ricketts@me.com>
2022-02-02 18:32:08 -0500

    might be the last iteration on saddle points

--- java/saddle-points/src/main/java/Matrix.java
+++ java/saddle-points/src/main/java/Matrix.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Matrix {
 
@@ -21,7 +17,7 @@ public class Matrix {
             int numberOfElementsInRow = row.size();
             for (int columnIndex = 0; columnIndex < numberOfElementsInRow; columnIndex++) {
                 int matrixValue = this.matrix.get(rowIndex).get(columnIndex);
-                if (this.isRowSaddlePoint(matrixValue, row) && this.isColumnSaddlePoint(matrixValue, columnIndex)) {
+                if (this.isSaddlePoint(matrixValue, columnIndex, row)) {
                     saddlePoints.add(new MatrixCoordinate(rowIndex + 1, columnIndex + 1));
                 }
             }
@@ -29,40 +25,9 @@ public class Matrix {
         return saddlePoints;
     }
 
-    private boolean isColumnSaddlePoint(int value, int columnIndex) {
-        return this.matrix.stream().map(row -> row.get(columnIndex)).allMatch(i -> value <= i);
-    }
+    private boolean isSaddlePoint(int matrixValue, int columnIndex, List<Integer> row) {
+        return row.stream().allMatch(i -> matrixValue >= i) &&
+                this.matrix.stream().map(matrixRow -> matrixRow.get(columnIndex)).allMatch(i -> matrixValue <= i);
 
-    private boolean isRowSaddlePoint(int value, List<Integer> row) {
-        return row.stream().allMatch(i -> value >= i);
     }
 }
-
-
-/*
-    Algorithm
-    1.  Do by loop first then convert using streams if it makes sense
-    2.  create an empty set of Matrix Coordinates
-    3.  iterate through the entire matrix
-        1.  outer loop which iterates through rows
-        2.  inner loop which iterates through each element of a given row
-        3.  for each element in a row
-            1.  Look at the row values first
-                1.  collect all values in the row which are not the current elements index.
-                2.  If the element is greater than or equal to every element in the row, retain the value.
-                3.  If the element passes the row criteria for a saddle point, we need to look at the columns
-                    if not then we can move onto the next row element.
-            2.  If the row criteria is meet then we need to look at the column
-                1.  We should create a list of column elements.
-                2.  In so doing we need to keep track of where the element is in the colum by its index.
-                3.  We can then apply the same algorithm for the rows to the columns except we check that the element
-                is less than or equal to each element in the column.
-                4.  If the column criteria fails move onto the next element in the row.
-            3.  If the element meets both row and column criteria add it to the Set
-
-    1  2  3
-  |---------
-1 | 9  8  7
-2 | 5  3  2     <--- saddle point at column 1, row 2, with value 5
-3 | 6  6  7
-*/
