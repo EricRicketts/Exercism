f01c861f1070f86eb2e19bedb52a0fae1aaf5877
Eric Ricketts <eric_ricketts@me.com>
2022-02-15 14:00:38 -0500

    Javascript Syllabus Sets Ozan's Playlist

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,64 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  return Array.from(new Set(playlist));
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  const uniqueTracks = removeDuplicates(playlist);
+  return uniqueTracks.includes(track);
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  const uniqueTracks = removeDuplicates(playlist);
+  if (!uniqueTracks.includes(track)) uniqueTracks.push(track);
+  return uniqueTracks;
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+    let playlistSet = new Set(playlist);
+    if (playlistSet.has(track)) playlistSet.delete(track);
+    return Array.from(playlistSet);
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+     let allArtists = playlist.map(songAndArtist => songAndArtist.split("-")[1].trim());
+     return removeDuplicates(allArtists);
+}
