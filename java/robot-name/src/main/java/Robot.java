ed424d3e9fc44085b825241d7645e808cea29dfa
Eric Ricketts <eric_ricketts@me.com>
2022-02-16 16:37:10 -0500

    Java Track Robot Name

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,16 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Robot {
 
     private String name;
-    public Robot() {
-        this.name = null;
-    }
+    private static Set<String> allNamesSet = new HashSet<>();
 
     public String getName() {
         if (this.name == null) { this.name = generateTwoRandomLetters() + generateThreeRandomDigits(); }
+        if (!allNamesSet.isEmpty()) {
+            while (allNamesSet.contains(this.name)) {
+                this.name = generateTwoRandomLetters() + generateThreeRandomDigits();
+            }
+            allNamesSet.add(this.name);
+        } else {
+            allNamesSet.add(this.name);
+        }
         return this.name;
     }
 
@@ -19,19 +25,26 @@ public class Robot {
     }
 
     private String generateThreeRandomDigits() {
+        final String digits = "0123456789";
+        StringBuilder threeDigits = new StringBuilder();
         Random r = new Random();
 
+        for (int count = 0; count < 3; count++) {
+            threeDigits.append(digits.charAt(r.nextInt(digits.length())));
+        }
+
+        return threeDigits.toString();
     }
 
     private String generateTwoRandomLetters() {
         String capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
-        String twoLetters = "";
+        StringBuilder twoLetters = new StringBuilder();
         Random r = new Random();
 
         for (int count = 0; count < 2; count++) {
-            twoLetters += capitalLetters.charAt(r.nextInt(capitalLetters.length()));
+            twoLetters.append(capitalLetters.charAt(r.nextInt(capitalLetters.length())));
         }
 
-        return twoLetters;
+        return twoLetters.toString();
     }
 }
